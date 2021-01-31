558a4906c861853a459902099d8c73212fb56170
Austin Clyde <aclyde@uchicago.edu>
2021-01-31 10:01:04 -0600

    message

--- DataPrep.py
+++ DataPrep.py
@@ -285,5 +285,6 @@ def train_test_split(datafile, file_base_paths='data', sampler = 1.0, test_size 
 
 
 if __name__ == '__main__':
-    filename = "savi1to10_extended.txt"
-    train_test_split(filename, file_base_paths='data_subsample', sampler=0.1)
+    # filename = "savi1to10_extended.txt"
+    # train_test_split(filename, file_base_paths='data_subsample', sampler=0.1)
+    main()
