ab02b56dfff81e1fd2b95bffe1e9679f691c3213
Austin Clyde <aclyde@uchicago.edu>
2021-05-26 13:29:50 -0500

    message

---
+++ validate.py
@@ -0,0 +1,103 @@
+from rdkit import Chem
+import rdkit.Chem.Scaffolds.MurckoScaffold
+
+def mol_equal(s,t):
+    return s.HasSubstructMatch(t) and t.HasSubstructMatch(s)
+
+def check_scaffold(s,t):
+    s = Chem.MolFromSmiles(s)
+    t = Chem.MolFromSmiles(t)
+
+    if s is None or t is None:
+        return -1
+
+    scaffold = Chem.Scaffolds.MurckoScaffold.GetScaffoldForMol(s)
+
+    if mol_equal(scaffold, t):
+        return 1
+    else:
+        return 0
+
+def check_lower(s,t):
+    s = Chem.MolFromSmiles(s)
+    t = Chem.MolFromSmiles(t)
+
+    if s is None or t is None:
+        return -1
+    if t.HasSubstructMatch(s):
+        return 1
+    else:
+        return 0
+
+def check_upper(s,t):
+    s = Chem.MolFromSmiles(s)
+    t = Chem.MolFromSmiles(t)
+
+    if s is None or t is None:
+        return -1
+    if s.HasSubstructMatch(t):
+        return 1
+    else:
+        return 0
+    
+def check_expand(s,t):
+    s = Chem.MolFromSmiles(s)
+    t = Chem.MolFromSmiles(t)
+
+    if s is None or t is None:
+        return -1
+    scaffold = Chem.Scaffolds.MurckoScaffold.GetScaffoldForMol(t)
+
+    if mol_equal(scaffold, s):
+        return 1
+    else:
+        return 0
+
+def main(fsrc, ftgt):
+
+    total = 0
+    correct = 0
+    correct_syntax = 0
+    with open(fsrc, 'r') as src:
+        with open(ftgt, 'r') as tgt:
+
+            for s,t in zip(src, tgt):
+                op = s.split(" ")
+                s = " ".join(op[1:])
+                op = op[0]
+                s = s.strip().replace(' ', '')
+                t = t.strip().replace(' ', '')
+
+                if op == 'SCAFFOLD':
+                    r = check_scaffold(s,t)
+                elif op == 'EXPAND':
+                    r  = check_expand(s,t)
+                elif op == 'LOWER':
+                    r = check_lower(s,t)
+                elif op == 'UPPER':
+                    r = check_upper(s,t)
+                else:
+                    print(f"Error {op} did not match anything. The whole line is {s,t}")
+                    exit()
+
+                total += 1
+                if r == 0: #could parse, but not right
+                    correct_syntax += 1
+                if r > 0:
+                    correct += 1
+
+        print(f"Total {total}, Syntax good {correct_syntax} ({correct_syntax /total}), Correct {correct} ({correct} / {total}) ")
+
+
+
+
+
+
+if  __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-src', type=str, required=True)
+    parser.add_argument('-tgt', type=str, required=True)
+    args = parser.parse_args()
+
+    main(args.src, args.tgt)
